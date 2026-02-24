69da6b05de72570a2aee1e9a7526002cf9a453c5
George Sowles <sowlesgeorge@gmail.com>
2026-02-23 22:06:24 -0500

    3 dimensional vector addtion example added

---
+++ 3_dimensional_vector_sum.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void vecAdd(float* A_h, float* B_h, float* C_h, int n) {
+	for (int i = 0 ;i< n; ++i){
+		C_h[i] = A_h[i] + B_h[i];
+	}
+}
+
+int main(int argc, char* argv[]) {
+	int n = atoi(argv[1]);
+
+	float* A_h = (float*)malloc(n * sizeof(float));
+	float* B_h = (float*)malloc(n * sizeof(float));
+	float* C_h = (float*)malloc(n * sizeof(float));
+
+	for (int i = 0; i < n; ++i)
+		scanf("%f", &A_h[i]);
+
+	for (int i = 0; i < n; ++i)
+		scanf("%f", &B_h[i]);
+
+	vecAdd(A_h, B_h, C_h, n);
+
+	for (int i = 0; i < n; ++i)
+		printf("%f ", C_h[i]);
+	printf("\n");
+
+	free(A_h);
+	free(B_h);
+	free(C_h);
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # cudacoral
+This is my attempt to learn Cuda C with my Nvidia GT 730. Most of these exercises are taken directly from *Programming Massively Parallel Processors* by David B. Kirk and Wen-mei W. Hwu 
